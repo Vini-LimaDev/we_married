2e5cf4bfa82491cb7c87531f340d5ec47ae9b879
vinicius <viniciusalmeidalima@icloud.com>
2026-08-03 21:43:39 -0300

    Remove Countdown, Evento, Footer, Hero, Historia, Navigation, Notification, Presentes, RSVP components and their associated styles and API services.

--- frontend/src/components/Notification.jsx
+++
@@ -1,9 +0,0 @@
-import './Notification.css'
-
-export default function Notification({ notification }) {
-  return (
-    <div className={`notification ${notification.show ? 'show' : ''}`}>
-      {notification.message}
-    </div>
-  )
-}
